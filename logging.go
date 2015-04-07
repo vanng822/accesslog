40bcf249b3d4b4de53b6842a0e6150f0106edf07
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2015-04-07 09:06:44 +0200

    Move doc to own file

--- logging.go
+++ logging.go
@@ -1,23 +1,3 @@
-// Package accesslog logs request in standard format http://en.wikipedia.org/wiki/Common_Log_Format using interface Logger.Printf(format string, v ...interface{})
-//
-//	package main
-//	
-//	import (
-//		"fmt"
-//		"github.com/vanng822/accesslog"
-//		"github.com/vanng822/r2router"
-//		"net/http"
-//	)
-//	
-//	func main() {
-//		seefor := r2router.NewSeeforRouter()
-//		log := accesslog.NewLog()
-//		seefor.Before(log.Handler)
-//		seefor.Get("/user/keys/:id", func(w http.ResponseWriter, r *http.Request, p r2router.Params) {
-//			fmt.Fprint(w, p.Get("id"))
-//		})
-//		http.ListenAndServe(":8080", seefor)
-//	}
 package accesslog
 
 import (
